25dda3d52bd9f57b6f152f75eec0f3de64d86d49
Ajax23 <github@kraushh.de>
2021-03-05 11:18:20 +0100

    Add missing tests

--- tests/test_simple.py
+++ tests/test_simple.py
@@ -29,6 +29,29 @@ class UserModelCase(unittest.TestCase):
             elif os.path.isdir(file_path):
                 shutil.rmtree(file_path)
 
+    #########
+    # Utils #
+    #########
+    def test_utils(self):
+        file_link = "output/test/test.txt"
+
+        hga.utils.mkdirp("output/test")
+
+        self.assertEqual(hga.utils.column([[1, 1, 1], [2, 2, 2]]), [[1, 2], [1, 2], [1, 2]])
+
+        hga.utils.save([1, 1, 1], file_link)
+        self.assertEqual(hga.utils.load(file_link), [1, 1, 1])
+
+        self.assertEqual(round(hga.utils.mumol_m2_to_mols(3, 100), 4), 180.66)
+        self.assertEqual(round(hga.utils.mols_to_mumol_m2(180, 100), 4), 2.989)
+        self.assertEqual(round(hga.utils.mmol_g_to_mumol_m2(0.072, 512), 2), 0.14)
+        self.assertEqual(round(hga.utils.mmol_l_to_mols(30, 1000), 4), 18.066)
+        self.assertEqual(round(hga.utils.mols_to_mmol_l(18, 1000), 4), 29.8904)
+
+        print()
+        hga.utils.toc(hga.utils.tic(), message="Test", is_print=True)
+        self.assertEqual(round(hga.utils.toc(hga.utils.tic(), is_print=True)), 0)
+
 
     ################
     # Cyclodextrin #
@@ -39,14 +62,10 @@ class UserModelCase(unittest.TestCase):
         sns.set_context("paper")
         sns.set_palette(sns.color_palette("deep"))
 
-        # Plot histogram
-        hga.affinity.hist("data/COLVAR", [1, 2], ["Centers of Mass", "Oxygenes"], conditions={2: [1, 0.5]})
-        plt.savefig("output/affinity.pdf", format="pdf", dpi=1000)
-        # plt.show()
-
         # Count bound and unbound instances
         print()
         hga.affinity.sample("data/COLVAR", "output/count.obj", [1, 0.5], [2, 0.46], is_force=True)
+        hga.affinity.sample("data/COLVAR", "output/count.obj", [1, 0.5], [2, 0.46], is_force=False)
 
         # Calculate binding affinity through brute-force summation
         print()
@@ -59,6 +78,23 @@ class UserModelCase(unittest.TestCase):
         table = pd.concat(tables)
         print(table)
 
+        # Test standard deviation
+        print()
+        tables = [hga.affinity.time("output/count.obj", cutoff, 298.15, 31.3707e-27, is_std=True) for cutoff in [100*x for x in range(11)]]
+        table = pd.concat(tables)
+        print(table)
+
+        # Plot histogram
+        plt.figure(figsize=(6, 4))
+        hga.affinity.hist("data/COLVAR", [1, 2], ["Centers of Mass", "Oxygenes"], conditions={2: [1, 0.5]})
+        plt.savefig("output/affinity.pdf", format="pdf", dpi=1000)
+        # plt.show()
+
+        # Plot time series
+        plt.figure(figsize=(6, 4))
+        hga.affinity.time_series("data/COLVAR", [1, 2])
+        plt.savefig("output/time_series.pdf", format="pdf", dpi=1000)
+        # plt.show()
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
